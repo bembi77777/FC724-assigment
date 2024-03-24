fc3477e0debcbd14a2b13db15e945363a830b532
binderya9575 <binderya9575@gmail.com>
2024-03-24 18:16:31 +0000

    second commit created class

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, redirect, url_for
+from flask import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, RadioField
+from wtforms.validators import InputRequired
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'blank'
+
+class Survey(FlaskFrom):
+    name = StringField('Name', validators=[InputRequired()])
+    student_number = StringField('Student number', validators=[InputRequired()])
+    email = StringField('Email', validators=[InputRequired()])
+    grades = StringField('Grades', validators=[InputRequired()])
+    satisfaction = SelectField('Overall Satisfaction', choices=[
+        ('', 'Select Satisfaction Level'),
+        ('excellent', 'Excellent'),
+        ('good', 'Good'),
+        ('average', 'Average'),
+        ('poor', 'Poor')
+    ])
+    suggestions = TextAreaField('Suggestions For Improvement', validators=[InputRequired()])
 
 @app.route('/home')
 def home():
     return render_template('index.html')
+
 
 @app.route('/information')
 def information():
